c7531da65a19ae1ca1cc51c8bdb878ff65114aec
Ceren Kaya <cerenkaya1997ck@hotmail.com>
2024-02-22 12:23:57 +0300

    put,push,delete

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,7 +1,15 @@
+using TodoApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//tüm izinler verildi.
+builder.Services.AddCors(s => s.AddDefaultPolicy(
+    p => p.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
+
+builder.Services.AddDbContext<TodoContext>(builder => builder.UseInMemoryDatabase("TodoList"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,10 +30,22 @@ app.UseHttpsRedirection();
 app.UseDefaultFiles();
 
 //statik dosyalarýn dýþarý açýlmasý(wwworoot)
-app.UseStaticFiles(); 
+app.UseStaticFiles();
+
+//api izni için 
+app.UseCors();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<TodoContext>();
+    db.Database.EnsureCreated();
+}
+
 app.Run();
+
+// Install-Package Microsoft.EntityFrameworkCore
+// Install-Package Microsoft.EntityFrameworkCore.InMemory
